dfc74b5fb87169745d314e5e99edab45c9f381d1
Adam <nawrotadam02@gmail.com>
2022-02-20 22:03:34 +0100

    Gradle initial file structure

---
+++ src/main/java/com/practice/texthero/TextHeroApplication.java
@@ -0,0 +1,13 @@
+package com.practice.texthero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextHeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextHeroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/texthero/TextHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.texthero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextHeroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
